2a674d77aaa210267dc72f698ab6cc6a3c38b61c
songb <songb@esumtech.com>
2021-04-23 11:30:35 +0900

    first commit

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>TODO it!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    h1 {
+        margin: 2.5rem 1.5rem;
+        color: #2f3b52;
+        font-weight: bold;
+    }
+</style>
